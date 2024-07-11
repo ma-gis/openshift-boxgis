eb655841b43b3137c9d81807ca7649c806fe2a66
ma-gis <mahtab.gis@gmail.com>
2024-07-10 22:47:16 -0500

    environment settings

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from functools import lru_cache
+from typing import Annotated
 import uvicorn
+from config import Settings
 
 app = FastAPI()
+
+@lru_cache
+def get_settings():
+    return Settings()
 
 @app.get('/')
 def index():
@@ -10,6 +17,14 @@ def index():
 @app.get('/message')
 def index():
     return {'data': 'FastAPI is great!'}
+
+@app.get("/info")
+async def info(settings: Annotated[Settings, Depends(get_settings)]):
+    return {
+        "app_name": settings.app_name,
+        "admin_email": settings.admin_email,
+        "items_per_user": settings.items_per_user,
+    }
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8080)
