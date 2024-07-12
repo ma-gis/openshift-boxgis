b1d117d8a7ce30ebb7adc1b3c0cf573ec1ea26a6
ma-gis <mahtab.gis@gmail.com>
2024-07-11 20:31:45 -0500

    test settings

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "Awesome API"
-    admin_email: str
+    admin_email: str ="some@one.com"
     items_per_user: int = 50
